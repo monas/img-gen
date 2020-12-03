c3111d9e7388a406ed094aa4166ede66a4cbeec9
Laimonas <monazzz@gmail.com>
2020-12-03 18:02:09 +0200

    Pirmas variantas Projekto MVP v1

---
+++ src/index.js
@@ -0,0 +1,42 @@
+(function () {
+  const tekstai = ["Labas mama", "Kaip tau sekasi?", "Tuoj Tuoj Tuoj"];
+
+  let atsitiktinisTekstas = atrinkTeksta(tekstai);
+
+  keiciamTeksta(atsitiktinisTekstas);
+})();
+
+/**
+ * Priskirs elementui nauja teksta
+ *
+ * @param {string} tekstas
+ */
+function keiciamTeksta(tekstas = "noriu teksto :O") {
+  let tekstoVieta = document.getElementById("tekstas");
+
+  tekstoVieta.innerHTML = tekstas;
+}
+
+/**
+ * Grazins viena teksta is masyvo
+ *
+ * @param {array} galimosReiksmes
+ */
+function atrinkTeksta(galimosReiksmes = []) {
+  if (!galimosReiksmes.length) {
+    return "nera iš ko rinktis";
+  }
+
+  let atstitiktineReiksme = gautAtsitiktiniSkaiciu(galimosReiksmes.length);
+
+  return galimosReiksmes[atstitiktineReiksme];
+}
+
+/**
+ * Grazins atstitiktini skaiciu iki max
+ *
+ * @param {number} max
+ */
+function gautAtsitiktiniSkaiciu(max) {
+  return Math.floor(Math.random() * Math.floor(max));
+}
